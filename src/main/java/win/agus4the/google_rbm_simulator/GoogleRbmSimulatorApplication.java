068ba1087acc79ce0d19241a46ce8bc691e4b322
Agustín López Fioravantti <agustinlopezfioravantti@gmail.com>
2025-08-11 10:56:23 +0200

    Add files via upload

---
+++ src/main/java/win/agus4the/google_rbm_simulator/GoogleRbmSimulatorApplication.java
@@ -0,0 +1,13 @@
+package win.agus4the.google_rbm_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleRbmSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleRbmSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/win/agus4the/google_rbm_simulator/GoogleRbmSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package win.agus4the.google_rbm_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleRbmSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
